c969083a8f86cbf48ba577b702fe82ff7d45eb2c
Luiz Felipe S <70417435+Luiz-Felipe123@users.noreply.github.com>
2023-12-09 20:10:21 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const fs = require("fs");
 const fs = require("fs").promises;
 const { v4: uuidv4 } = require('uuid');
 
