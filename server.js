1ab7bb4a66198f545402f83c495983fe336aec3c
Luiz Felipe S <70417435+Luiz-Felipe123@users.noreply.github.com>
2023-12-09 23:28:48 -0300

    Update server.js

--- server.js
+++ server.js
@@ -99,9 +99,3 @@ app.delete("/students/:id", (req, res) => {
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
-
-
-// Inicializa o servidor
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta ${port}`);
-});
